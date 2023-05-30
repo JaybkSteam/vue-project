b0d6da0115acaabf24bcfe516d0782d21b0d48a9
Jaybksteam <jedouard@brooklynsteamcenter.org>
2023-05-30 14:56:59 -0400

    changes

---
+++ src/router.js
@@ -0,0 +1,35 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+
+createApp(App).use(vuetify).use(router).mount('#app')
+
+import {createRouter, createWebHashHistory}  from  'vue-router'
+import Homepage from './views/Homepage.vue' 
+
+const routes = [
+  {
+    path: '/' ,
+    component: Homepage
+  },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+export default router
+
